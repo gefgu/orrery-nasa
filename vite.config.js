b84e53b0005c69906c92b4715aa6ce44e89fb524
Gustavo Santos <gefgu@hotmail.com>
2024-10-06 15:07:24 -0300

    feat: remove static copy

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react(), viteStaticCopy({
-    targets: [
-      {
-        src: 'webapp.html', // source file in the root directory
-        dest: '.'           // destination folder inside dist (root of dist)
-      }
-    ]
-  })],
+  plugins: [react()],
   server: {
     host: '127.0.0.1', // Optional but recommended
     port: 3000,        // Use a different port (e.g., 3000)
