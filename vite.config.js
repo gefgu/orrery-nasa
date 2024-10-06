4a218b46da6cd3675232b818d9e787f33c63fd94
Gustavo Santos <gefgu@hotmail.com>
2024-10-06 14:19:04 -0300

    feat: try deployment with vite static

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      {
+        src: 'webapp.html', // source file in the root directory
+        dest: '.'           // destination folder inside dist (root of dist)
+      }
+    ]
+  })],
   server: {
     host: '127.0.0.1', // Optional but recommended
     port: 3000,        // Use a different port (e.g., 3000)
