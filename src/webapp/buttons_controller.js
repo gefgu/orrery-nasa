0a3800730077ec77953097650b794bfe8ca89a85
Gustavo Santos <gefgu@hotmail.com>
2024-10-06 19:18:00 -0300

    feat: toggle storyless

--- src/webapp/buttons_controller.js
+++ src/webapp/buttons_controller.js
@@ -12,6 +12,19 @@ function handle_toggles(comets) {
       comet.orbitLine.visible = !comet.orbitLine.visible; // Toggle visibility
     });
   });
+
+  // Toggle stories
+  document.getElementById('toggle-stories').addEventListener('click', () => {
+    comets.forEach(comet => {
+      // Check if the comet does not have a "story_0" key
+      if (!comet.hasOwnProperty('story_0')) {
+        comet.comet_object.visible = !comet.comet_object.visible; // Toggle visibility
+        comet.nameSprite.visible = !comet.nameSprite.visible; // Toggle visibility
+        comet.orbitLine.visible = !comet.orbitLine.visible; // Toggle visibility
+      }
+    });
+  });
+
 }
 
 export { handle_toggles };
